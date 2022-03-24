02040a58606d4e365cdcb20195a9a1259cc5e19c
alisson.sganzerla <alisson.sganzerla@gmailcom>
2022-03-24 09:19:01 -0300

    chore: add instances twente-university

--- Console-XHSTT/Console_XHSTT.py
+++ Console-XHSTT/Console_XHSTT.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 
+# http://jeffreykingston.id.au/khe/
 instances_khe = [
     "./instances/KHE/2014_08_19_KHE14x8_solns.xml",    
     "./instances/KHE/AU-BG-98_soln.xml", 
@@ -8,7 +9,15 @@ instances_khe = [
     "./instances/KHE/khe14_solns.xml"
     ]
 
-files = instances_khe
+# https://www.utwente.nl/en/eemcs/dmmp/hstt/datasets/
+instances_tu = [
+    "./instances/twente-university/BrazilInstance2.xml",
+    "./instances/twente-university/CzechVillageSchool.xml",
+    "./instances/twente-university/DenmarkFalkonergaardensGymnasium2012.xml",
+    "./instances/twente-university/ItalyInstance1.xml",
+    ]
+
+files = instances_khe + instances_tu
 filters = ["Times", "Resources", "Events", "Constraints"]
 
 def parse_xml(xml_file):
